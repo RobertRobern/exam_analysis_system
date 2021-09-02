5e5d5a2c98dcaae0967d7c45861a06b81829290f
RobertRobern <ndolorobern@gmail.com>
2021-09-02 14:40:40 +0300

    Add Factrory(Permission&Role),edited UserFactory

---
+++ database/factories/PermissionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Permission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PermissionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Permission::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->jobTitle,
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->paragraph(),
+            'active' => random_int(0,1),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Integer;
 
 class UserFactory extends Factory
 {
@@ -28,6 +30,13 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'roles_id'  => Role::factory(),
+            'dob' => now(),
+            'gender'  => 'Male',
+            'idnumber' => random_int(0,9),
+            'telnumber' => random_int(0,9),
+            'lastlogin' => now(),
+            'intro' => $this->faker->paragraph(),
         ];
     }
 
